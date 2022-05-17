cc124c8fce3c398a6507d008df955ea7c58c970e
Mary <marianacavalle1@gmail.com>
2022-05-17 09:15:57 -0300

    ajustes

---
+++ Aula 9/javascript/soma.js
@@ -0,0 +1,5 @@
+function Soma(){
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+    document.getElementById("res").innerHTML = "Resposta " + (n1 + n2);
+}
